f4513f1b60cb3a299e6c4f5128732268c4ff9019
mehedi09 <mehedi_aiub@hotmail.com>
2014-08-23 18:14:27 +0600

    1st task done.

---
+++ contact.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Style Play</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="author" content="Mehedi Hasan">
+	<meta name="description" content="Aim is to make people more fashionable. The Designs are elegant than other brands of Europe. Moreover, we are going to offer one of the best quality in a very cheap price. Hopefully our products will bring new fashion in our society">
+	<meta name="robots" content="all">
+	<link rel="stylesheet" type="text/css" href="css/spacelab-bootstrap-min-css/spacelab-bootstrap-min.css">
+	<link rel="stylesheet" type="text/css" href="css/my-bootstrap-min.css">
+
+<!--
+<link rel="stylesheet" type="text/css" href="http://bootswatch.com/spacelab/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="http://bootswatch.com/spcelab/bootstrap.min.css">C:\wamp\www\BASIS_Responsive\BASIS-Project\spacelab-bootstrap.min.css !-->
+
+</head>
+<body>
+
+	
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Style Play</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="author" content="Mehedi Hasan">
+	<meta name="description" content="Aim is to make people more fashionable. The Designs are elegant than other brands of Europe. Moreover, we are going to offer one of the best quality in a very cheap price. Hopefully our products will bring new fashion in our society">
+	<meta name="robots" content="all">
+	<link rel="stylesheet" type="text/css" href="css/spacelab-bootstrap-min-css/spacelab-bootstrap-min.css">
+	<link rel="stylesheet" type="text/css" href="css/my-bootstrap-min.css">
+
+<!--
+<link rel="stylesheet" type="text/css" href="http://bootswatch.com/spacelab/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="http://bootswatch.com/spcelab/bootstrap.min.css">C:\wamp\www\BASIS_Responsive\BASIS-Project\spacelab-bootstrap.min.css !-->
+
+</head>
+<body>
+	<header class="container">
+		<div class="mypage-header">
+					<!-- Company name or Logo-->
+		<h1>Style Play</h1>
+
+			<!-- Menu Bar-->
+		</div>	
+		<div class="navbar navbar-default">
+  <div class="navbar-header">
+
+    <a class="navbar-brand" href="#">Home</a>
+    <a class="navbar-brand" href="#">Gents</a>
+    <a class="navbar-brand" href="#">Ladies</a>
+    <a class="navbar-brand" href="#">Contact</a>
+  </div>
+  </div>
+		</header>
+
+
+	</header>
+	
+	<section class="container" id="main">
+		<section class="col-md-6">
+
+			<div class="row visible-md visible-lg" id="slider1_container" style="position: relative; width: 600px; height: 300px;">
+	 <!-- Loading Screen -->
+        <div u="loading" style="position: absolute; top: 0px; left: 0px;">
+            <div style="filter: alpha(opacity=70); opacity:0.7; position: absolute; display: block;
+                background-color: #000; top: 0px; left: 0px;width: 100%;height:100%;">
+            </div>
+            <div style="position: absolute; display: block; background: url(/img/loading.gif) no-repeat center center;
+                top: 0px; left: 0px;width: 100%;height:100%;">
+            </div>
+        </div>
+        <!-- Slides Container -->
+       	 <div u="slides" role="slider" style="cursor: move; position: absolute; left: 40px; top: 0px; width: 600px; height: 320px;
+            overflow: hidden;">
+	            <div>
+					<img u="image" src="img/slider/landscape/01.jpg" class="img-thumbnail"  />
+					<div u="caption" t="L" style="position: absolute; top: 20px; left: 20px; width: 200px; height: 30px; color: #ffffff; font-size: 20px; line-height: 30px;"><h2>Welcome To</h2>
+					</div>
+                <div u="caption" t="CLIP|LR" style="position: absolute; top: 200px; left: 100px; width: 400px; height: 60px; color: #ffffff; font-size: 46px; line-height: 40px; text-align: center;">Style Play</div>
+            
+				</div>
+	            <div>
+					<img u="image" src="img/slider/landscape/02.jpg" class="img-thumbnail"  />
+				</div>
+	            <div>
+					<img u="image" src="img/slider/landscape/03.jpg" class="img-thumbnail"  />
+				</div>
+	            <div>
+					<img u="image" src="img/slider/landscape/04.jpg" class="img-thumbnail" />
+				</div>
+	            <div>
+					<img u="image" src="img/slider/landscape/05.jpg" class="img-thumbnail" />
+				</div>
+	            <div>
+					<img u="image" src="img/slider/landscape/06.jpg" class="img-thumbnail"  />
+				</div>
+	            <div>
+					<img u="image" src="img/slider/landscape/07.jpg" class="img-thumbnail" />
+				</div>
+	            <div>
+					<img u="image" src="img/slider/landscape/08.jpg" class="img-thumbnail" />
+				</div>
+       		 </div>
+   			 </div>
+		
+
+		</section>
+
+
+
+
+		<section class="col-md-6">
+			<div class="jumbotron">
+				  <h1>Style Play</h1>
+				  <p>Our aim is to make people more fashionable. Our designs are elegant than other brands of Europe. Moreover, We are going to offer one of the best quality in a very cheap price. </p>
+				  <p><a class="btn btn-primary btn-lg">About Us</a></p>
+			</div>
+	</section>
+	</section>
+		
+
+	
+
+	
+	
+	<footer class="container">
+		<hr>
+	Copyright &copy; 2014 - All Rights Reserved - Style Play
+	</footer>
+
+<!--<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script type="text/javascript" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script> !-->
+ <script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
+    <!-- use jssor.slider.mini.js (39KB) or jssor.sliderc.mini.js (31KB, with caption, no slideshow) or jssor.sliders.mini.js (26KB, no caption, no slideshow) instead for release -->
+    <!-- jssor.slider.mini.js = jssor.sliderc.mini.js = jssor.sliders.mini.js = (jssor.core.js + jssor.utils.js + jssor.slider.js) -->
+    <script type="text/javascript" src="js/jssor.core.js"></script>
+    <script type="text/javascript" src="js/jssor.utils.js"></script>
+    <script type="text/javascript" src="js/jssor.slider.js"></script>
+	
+    <script type="text/javascript" src="js/my-slider.js"></script>
+ <script type="text/javascript" src="js/my-slider.js"></script>
+</body>
+</html>
